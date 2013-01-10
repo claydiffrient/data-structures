1759d4112a6c8c8349701ce3a71c91d9d65d2399
Clay Diffrient <clay.diffrient@gmail.com>
2013-01-10 08:25:23 -0700

    Fixed build, changed directory structure.

---
+++ linkedlist/c++/main.cpp
@@ -0,0 +1,11 @@
+/*******************************************************************************
+* Main for testing purposes.
+*******************************************************************************/
+#include "Node.cpp"
+
+int main(int argc, char const *argv[])
+{
+   Node<int>* myNode;
+   myNode = new Node<int>();
+   return 0;
+}
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-linkedlist : linkedlist.cpp
-	g++ -o linkedlist linkedlist.cpp
+node : ./linkedlist/c++/Node.cpp ./linkedlist/c++/Node.h ./linkedlist/c++/main.cpp
+	g++ -o node ./linkedlist/c++/main.cpp
 clean:
-	rm linkedlist
+	rm node
